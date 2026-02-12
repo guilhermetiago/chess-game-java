7098bdffb07aa25084c5a4591763d579acf13035
guilhermetiago <guilherme.tiagoap@gmail.com>
2026-02-12 10:21:16 -0300

    implementação de board e piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
 
-        Position pos = new Position(2, 6);
-
-        System.out.println(pos.toString());
+        Board board = new Board(8, 8);
+        
 
     }
 
