2f38f965bfbce10b250e0f2e0c9bb581eee79219
guilhermetiago <guilherme.tiagoap@gmail.com>
2026-03-04 23:15:03 -0300

    Contagem de movimentos

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -17,9 +18,22 @@ public abstract class ChessPiece extends Piece {
         return ChessPosition.fromPosition(position);
     }
 
+    public int getMoveCount() {
+        return moveCount;
+    }
+
     public Color getColor() {
         return color;
     }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
 
     protected boolean isThereOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece)getBoard().piece(position);
