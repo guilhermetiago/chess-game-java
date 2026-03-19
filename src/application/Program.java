91d28a9c2ea889ef3c7197d72e8c030b639be955
guilhermetiago <guilherme.tiagoap@gmail.com>
2026-03-19 14:50:29 -0300

    Colocando o movimento especial promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -38,6 +39,12 @@ public class Program {
 
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
+                }
+
+                if(chessMatch.getPromoted() != null) {
+                    System.out.println("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
                 }
             }
             catch (ChessException e) {
